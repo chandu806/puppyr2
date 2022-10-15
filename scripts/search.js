16fbdc2ac3e2ade74ad4eb0e3fa5eb7a6f9c29d5
chandu806 <76252021+chandu806@users.noreply.github.com>
2022-10-15 12:14:20 +0530

    puppy

--- scripts/search.js
+++ scripts/search.js
@@ -5,12 +5,46 @@ const getSuggestions = async() => {
 
     const searchData = await fetch(`https://dog.ceo/api/breed/${breedname}/images`)
     .then(res => res.json())
-    // const myImage = new Image(100, 200);
-// myImage.src = 'picture.jpg';
-document.body.appendChild(searchData);
-tbody.append(searchData)
 
-    
+
+    let tBody = document.getElementById("tableBody")
+    if(searchData){
+        searchData.forEach(element => {
+            let tr = document.createElement("tr")
+            let td1 = document.createElement("td")
+            let td2 = document.createElement("td");
+            let td3 = document.createElement("td");
+            let td4 = document.createElement("td");
+            let button = document.createElement("button")
+            let a = document.createElement("a")
+
+            a.setAttribute("href", "singlePage.html")
+            td1.style.padding = "2%"
+            tr.style.background = "green"
+            tr.style.color = "#ffff"
+            tr.style.textAlign = "center"
+            td1.innerHTML = element.name;
+            td2.innerHTML = element.brewery_type;
+            td3.innerHTML = element.city;
+            td4.innerHTML = element.state;
+            a.innerHTML = "More details"
+            button.style.width = "90%";
+            button.style.padding = "5% 0"
+            button.style.marginTop = "16px"
+            button.style.fontSize = "16px"
+            button.style.background = "orange"
+            button.style.border = "none"
+            button.style.borderRadius = "5px"
+
+            button.addEventListener("click", function myFunction(){
+                localStorage.setItem("single-list", JSON.stringify(element))
+                console.log("clicked")
+            })
+            button.append(a)
+            tr.append(td1,td2,td3,td4,button)
+            tBody.append(tr)
+        });
+    }
     
     // console.log("Fetching Data...", counter++);
 }
